5d4aef5be3c27c4d9ce2b121020a74465fbe0e31
Metehan Atalay <147618606+Wexis39@users.noreply.github.com>
2024-07-01 16:54:42 +0300

    Add files via upload

---
+++ GUI Timer.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter.ttk import Combobox
+import time
+
+root = tk.Tk()
+root.geometry('242x140')
+
+root.config(bg='#fab7ef')
+
+def timer():
+    current_time = time.strftime('%H:%M:%S')
+    label_time.config(text=current_time)
+    label_time.after(1000,timer)
+
+label_time = tk.Label(root,bg='#b38be5',font='sans 45 bold',fg='#ffffff')
+label_time.pack(padx=0,pady=0)
+
+label = tk.Label(root,bg='#8f5bd1',font='sans 36 bold',fg='#ffffff',text='Timer')
+label.pack(padx=0,pady=0,fill='x')
+
+timer()
+
+root.mainloop()
